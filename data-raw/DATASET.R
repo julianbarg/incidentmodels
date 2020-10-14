8aeaea442ca8084d63663835fcedc3e4080cf9fb
Julian Barg <barg.julian@gmail.com>
2020-10-14 11:17:39 -0400

    Added html slides to present results.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
 
 system("jupyter nbconvert --to notebook --execute --inplace data-raw/util/1-topic_model.ipynb")
+system("jupyter nbconvert --to slides data-raw/util/1-topic_model.ipynb")
 system("jupyter nbconvert --to notebook --execute --inplace data-raw/util/2-interpret_topics.ipynb")
+system("jupyter nbconvert --to slides data-raw/util/2-interpret_topics.ipynb")
