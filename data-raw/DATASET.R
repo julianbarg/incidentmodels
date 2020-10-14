bbc857b4d49ea43cb4ff35aa26d763ce900d7d4d
Julian Barg <barg.julian@gmail.com>
2020-10-14 09:50:54 -0400

    Finished generating topics for incidents.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
-suppressMessages(library(here))
 
 system("jupyter nbconvert --to notebook --execute --inplace data-raw/util/1-topic_model.ipynb")
-# system("jupyter nbconvert --to notebook --execute --inplace data-raw/util/2-interpret_topics.ipynb")
+system("jupyter nbconvert --to notebook --execute --inplace data-raw/util/2-interpret_topics.ipynb")
